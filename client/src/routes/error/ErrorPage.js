de57ea39da4d1a09f505503674b4cd9a467a97e8
Vitalii Perehonchuk <brute18@gmail.com>
2019-08-30 11:00:26 +0300

    ls test

--- client/src/routes/error/ErrorPage.js
+++ client/src/routes/error/ErrorPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/withStyles';
 import s from './ErrorPage.css';
