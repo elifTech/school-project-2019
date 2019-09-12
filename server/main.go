7b8571791518ca782b188f8468c0b236958efcb9
polluxEl <yuriy.kosakivsky@eliftech.com>
2019-09-12 15:55:58 +0300

    commented unneeded Db conn

--- server/main.go
+++ server/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	//"github.com/jinzhu/gorm"
 	//_ "github.com/jinzhu/gorm/dialects/postgres"
 	"log"
 	"net/http"
 	"school-project-2019/server/domain"
-	"school-project-2019/server/storage"
+	//"school-project-2019/server/storage"
 )
 
 func main() {
 	// init DB
-	db, err := storage.Connect()
-	if err != nil {
-		log.Fatal(fmt.Printf("Error connecting: %v \n", err))
-		return
-	}
-	defer db.Close()
+	//db, err := storage.Connect()
+	//if err != nil {
+	//	log.Fatal(fmt.Printf("Error connecting: %v \n", err))
+	//	return
+	//}
+	//defer db.Close()
 
-	s := &domain.IoTService{DB: db}
-	s.DB.AutoMigrate()
+	//s := &domain.IoTService{DB: db}
+	//s.DB.AutoMigrate()
 	// init server
 	log.Fatal(http.ListenAndServe(":8080", domain.NewRouter()))
 }
